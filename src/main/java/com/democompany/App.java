871dd90b94ff3c08bb304dad6bc9f4af7b42b6bf
vishal <vishal@gmail.com>
2022-11-03 23:33:56 +0530

    added intital commit

---
+++ src/main/java/com/democompany/App.java
@@ -0,0 +1,13 @@
+package com.democompany;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
